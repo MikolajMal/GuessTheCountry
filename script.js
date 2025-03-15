27512ce829b64a8dfcd1e39ea6070b4950130ad7
Mikołaj Maliszewski <maliszewski.mik@gmail.com>
2025-03-15 14:24:45 +0100

    Remove old functionality

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 import { getRandomNamesAndFlags } from './gameSetup.js'
 
 let flagButton = document.getElementById('flagButton');
-let capitalButton = document.getElementById('capitalButton');
 let coatOfArmsButton = document.getElementById('coatOfArmsButton');
 
 flagButton.addEventListener('click', openFlagsGame);
-capitalButton.addEventListener('click', () => opengameSetup('Capitals'));
-coatOfArmsButton.addEventListener('click', () => opengameSetup('Coats of Arms'));
 
 function openFlagsGame() {
     let flagsGameCollection = getRandomNamesAndFlags(5);
@@ -15,10 +12,6 @@ function openFlagsGame() {
     window.location.href = 'flagsGame.html';
 }
 
-function opengameSetup(gameType) {
-    localStorage.setItem('gameType', gameType)
-    window.location.href = 'gameSetup.html';
-}
 const url = `https://restcountries.com/v3.1/all?fields=name,flags,capital,coatOfArms`;
 
 const imageContainers = document.getElementsByClassName('bannerImageContainer');
@@ -38,16 +31,14 @@ async function fetchCountries(){
 
 function setupCountries(data){
     flagsCollection = data;
-    let flag, capital, coatOfArms;
-    while (!(flag && capital && coatOfArms)) {
+    let flag, coatOfArms;
+    while (!(flag && coatOfArms)) {
         const random = Math.floor(Math.random() * data.length);
         flag = data[random].flags.svg;
-        capital = data[random].capital;
         coatOfArms = data[random].coatOfArms.svg;
     }
 
     flagButton.querySelector('img').src = flag;
-    capitalButton.querySelector('p').innerHTML = capital;
     coatOfArmsButton.querySelector('img').src = coatOfArms;
     Array.from(imageContainers).forEach(element => {
         for (let i = 0; i < 16; i++) {
